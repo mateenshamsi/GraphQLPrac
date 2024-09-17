947345b1115be9b98f2696e9c615aa085175479e
mateenshamsi <matinshamsi16@gmail.com>
2024-09-17 23:33:16 +0530

    Added prisma

---
+++ build/index.cjs
@@ -0,0 +1,52 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const server_1 = require("@apollo/server");
+const express4_1 = require("@apollo/server/express4");
+const db_1 = require("./lib/db.cjs");
+function start() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const app = (0, express_1.default)();
+        app.use(express_1.default.json());
+        const server = new server_1.ApolloServer({
+            typeDefs: `
+    type Query{ 
+        hello:String
+        say(name:String):String
+    }
+        type Mutation{ 
+            createUser(firstName:String!,lastName:String!,email:String!,password:String!):Boolean
+        }
+    `,
+            resolvers: {
+                Query: {
+                    hello: () => `Hey there I am grahql`,
+                    say: (_, { name }) => `hi ${name} `
+                },
+                Mutation: {
+                    createUser: (_1, _a) => __awaiter(this, [_1, _a], void 0, function* (_, { firstName, lastName, email, password }) {
+                        yield db_1.prismaClient.user.create({ data: { firstName, lastName, email, password, salt: "random_salt" } });
+                    })
+                }
+            }
+        });
+        yield server.start();
+        app.use('/graphql', (0, express4_1.expressMiddleware)(server));
+        app.listen(3000, () => {
+            console.log("listeniong on port 3000");
+        });
+    });
+}
+start();
